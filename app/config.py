f9cfcd4b7d99a7dab861b3ecbe544062a8befc71
Cam Nguyen Tran <camnguyenmeow@gmail.com>
2025-05-27 13:26:35 +0700

    feat: change path of checkpoints

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static')
+    DETECT_MODEL_PATH = os.getenv("DETECT_MODEL_PATH")
+    OCR_MODEL_PATH = os.getenv("OCR_MODEL_PATH")
     ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'heic'}
 
     MIN_LP_ACREAGE = 0.02
--- app/models.py
+++ app/models.py
@@ -1,11 +1,8 @@
 import torch
 import pillow_heif
-import os
 
-detect_path = os.getenv("DETECT_MODEL_PATH")
-ocr_path = os.getenv("OCR_MODEL_PATH")
 pillow_heif.register_heif_opener()
 
-yolo_LP_detect = torch.hub.load('yolov5', 'custom', path=detect_path, force_reload=True, source='local')
-yolo_license_plate = torch.hub.load('yolov5', 'custom', path=ocr_path, force_reload=True, source='local')
+yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='checkpoints/LP_detector.pt', force_reload=True, source='local')
+yolo_license_plate = torch.hub.load('yolov5', 'custom', path='checkpoints/LP_ocr.pt', force_reload=True, source='local')
 yolo_license_plate.conf = 0.60

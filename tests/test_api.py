818fad334d7caafe69fe4c050aa97a032023c028
cnmeow <camnguyenmeow@gmail.com>
2025-06-04 16:14:10 +0700

    fix: add requirement

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,6 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import io
 import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
 import shutil
